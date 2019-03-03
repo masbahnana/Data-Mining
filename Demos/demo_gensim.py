d2216bda8385b436431b559f11b1ea8dbeca3e59
Natalia Raythz <nraythz@gmail.com>
2019-03-03 15:47:39 -0300

    add mining codes

---
+++ Demos/demo_gensim.py
@@ -0,0 +1,10 @@
+from gensim.summarization import summarize
+import sys
+
+fname = sys.argv[1]
+
+with open(fname, 'r') as f:
+    content = f.read()
+    summary = summarize(content, split=True, word_count=100)
+    for i, sentence in enumerate(summary):
+        print("%d) %s" % (i+1, sentence))
---
+++ Facebook/facebook_my_profile.py
@@ -0,0 +1,11 @@
+import os
+import json
+import facebook
+
+if __name__ == '__main__':
+    token = os.environ.get('FACEBOOK_TEMP_TOKEN')
+
+    graph = facebook.GraphAPI(token)
+    profile = graph.get_object("me", fields='name,location{general_info,location},languages{name,description}')
+
+    print(json.dumps(profile, indent=4))
